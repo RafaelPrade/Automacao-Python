5f4b8e5732093ff27796a2f0ca9d5a5fa83219d0
Rafael Prade <105230045+RafaelPrade@users.noreply.github.com>
2024-03-27 17:07:25 -0300

    Delete pegar_posicao.py

--- pegar_posicao.py
+++
@@ -1,7 +0,0 @@
-import pyautogui
-import time
-
-time.sleep(5)
-print(pyautogui.position())
-
-# pyautogui.scroll(5000) - Caso precise rolar o scroll do mouse para a página do sistema ir para o topo e recomeçar o processo.
